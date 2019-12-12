0f459e11c0bbaac6802a22b92b97a952bcdffc6a
Robert Bakota <robakota@vsite.hr>
2019-12-12 20:26:31 +0100

    Resolve reference type porperty

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -5,7 +5,6 @@ namespace Vsite.CSharp.Svojstva
     // ilustracija kako svojstvo ne može biti tipa void i kako se ne mogu preopterećivati 
     class Preopterećenje
     {
-        // TODO:020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
         //public void VoidSvoj
         //{
         //}
@@ -16,7 +15,6 @@ namespace Vsite.CSharp.Svojstva
             get { return Math.PI; }
         }
 
-        // TODO:021 Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
         //public int PreopterećenoSvojstvo
         //{
         //    get { return 3; }
