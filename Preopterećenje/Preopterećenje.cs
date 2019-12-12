02d2b6e1950bcf6ccad7e56beb0e8168d2b489ad
Ivan Batinić <ivek81cro@gmail.com>
2019-12-12 20:16:15 +0100

    Resolve properties overloading

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -5,8 +5,11 @@ namespace Vsite.CSharp.Svojstva
     // ilustracija kako svojstvo ne može biti tipa void i kako se ne mogu preopterećivati 
     class Preopterećenje
     {
-        // TODO:020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
+        // property ne moze vracat void
+        //public void VoidSvoj
+        //{
 
+        //}
 
         // svojstvo se ne može preopterećivati
 
@@ -15,8 +18,10 @@ namespace Vsite.CSharp.Svojstva
             get { return Math.PI; }
         }
 
-        // TODO:021 Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
-
+        //public int PreopterecenoSvojstvo
+        //{
+        //    get { return 3; }
+        //}
 
 
 
