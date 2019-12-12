3dc98a3d1398cc3dad155fdc83e792efc0b9a15a
Marko Vitković <mavitkov@vsite.hr>
2019-12-12 20:15:42 +0100

    done preopterecenje

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -5,8 +5,11 @@ namespace Vsite.CSharp.Svojstva
     // ilustracija kako svojstvo ne može biti tipa void i kako se ne mogu preopterećivati 
     class Preopterećenje
     {
-        // TODO:020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
+       
+		//public void VoidSvojstvo
+		//{
 
+		//}
 
         // svojstvo se ne može preopterećivati
 
@@ -15,12 +18,14 @@ namespace Vsite.CSharp.Svojstva
             get { return Math.PI; }
         }
 
-        // TODO:021 Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
+
+		//public int PreopterećenoSvojstvo
+		//{
+		//	get { return 3; }
+		//}
 
 
-
-
-        static void Main(string[] args)
+		static void Main(string[] args)
         {
         }
     }
