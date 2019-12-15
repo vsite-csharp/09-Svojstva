82f77f19b66395ed3f089fed13634f28949d1632
ivbatini <ivbatini@vsite.hr>
2019-12-15 14:54:00 +0100

    Resolve properties with parameters (indexers)

--- Indekseri/Indekseri.cs
+++ Indekseri/Indekseri.cs
@@ -18,12 +18,24 @@ namespace Vsite.CSharp.Svojstva
             { 51000, "Rijeka" }
         };
 
-        // TODO:040 Definirati svojstvo s int kao parametrom koje će za zadani poštanski broj vratiti mjesto. Otkomentirati naredbu u metodi IspišiNazivMjesta, pokrenuti program i testove.
+        public string this[int index]
+        {
+            get
+            {
+                return popis[index];
+            }
+        }
 
-
-
-        // TODO:041 Definirati svojstvo sa string kao parametrom koje će za zadano mjesto vratiti poštanski broj. Otkomentirati naredbu u metodi IspišiPoštanskiBroj, pokrenuti program i testove.
-
+        public int this[string index]
+        {
+            get
+            {
+                if (popis.ContainsValue(index))
+                    return popis.FirstOrDefault(x => x.Value == index).Key;
+                else
+                    throw new Exception($"{index} does not exist in dictionary.");
+            }
+        }
 
 
     }
@@ -36,7 +48,7 @@ namespace Vsite.CSharp.Svojstva
         {
             try
             {
-                //Console.WriteLine("Poštanski broj {0} ima: {1}", poštanskiBroj, pb[poštanskiBroj]);
+                Console.WriteLine("Poštanski broj {0} ima: {1}", poštanskiBroj, pb[poštanskiBroj]);
             }
             catch (System.Exception e)
             {
@@ -48,15 +60,13 @@ namespace Vsite.CSharp.Svojstva
         {
             try
             {
-                //Console.WriteLine("Poštanski broj za {0} je: {1}", mjesto, pb[mjesto]);
+                Console.WriteLine("Poštanski broj za {0} je: {1}", mjesto, pb[mjesto]);
             }
             catch (System.Exception e)
             {
                 Console.WriteLine(e.Message);
             }
         }
-
-        // TODO:042 Pokrenuti testove (2 testa "TestIndekseri" moraju proći).
 
         static void Main(string[] args)
         {
