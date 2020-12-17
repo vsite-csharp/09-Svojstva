7f8e608c7bab2264b3f5e9c5177cca686a711497
ivana andelic <54909467+ivana474@users.noreply.github.com>
2020-12-17 20:19:20 +0100

    Resolve invalid properties

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -5,13 +5,13 @@ namespace Vsite.CSharp.Svojstva
     // ilustracija kako svojstvo ne može biti tipa void i kako se ne mogu preopterećivati 
     class Preopterećenje
     {
-        // TODO:020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
+        //  Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
 
         public double PreopterećenoSvojstvo
         {
             get { return Math.PI; }
         }
-        // TODO:021 Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
+        // Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
 
 
         static void Main(string[] args)
