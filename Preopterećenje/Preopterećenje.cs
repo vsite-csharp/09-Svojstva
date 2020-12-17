617d71648508f0e3133dd6ad1b3de79cc9e882a9
luxorus89 <jakov.lux@hotmail.com>
2020-12-17 20:42:44 +0100

    Resolve indexers

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -6,14 +6,15 @@ namespace Vsite.CSharp.Svojstva
     class Preopterećenje
     {
         // TODO:020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
-
+        //public void voidSvojstvo {get;set;}
         public double PreopterećenoSvojstvo
         {
             get { return Math.PI; }
         }
         // Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
-        {
-
+        //{
+       // return{}
+       //
 
         static void Main(string[] args)
         {
