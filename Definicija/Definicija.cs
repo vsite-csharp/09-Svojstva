7f79c2c6c34f4a3bfd5d19af7168bc0555046ca1
IvanLatincic <ivanlatincic10@gmail.com>
2021-12-16 20:46:04 +0100

    .

--- Definicija/Definicija.cs
+++ Definicija/Definicija.cs
@@ -12,74 +12,100 @@ namespace Vsite.CSharp.Svojstva
                 Prezime = prezime;
             }
 
-            // TODO:000 Javno dostupno polje Prezime nadomjestiti svojstvom (property) koje se izvan klase može samo čitati, a mijenjati se može samo iz klase.
+            // :000 Javno dostupno polje Prezime nadomjestiti svojstvom (property) koje se izvan klase može samo čitati, a mijenjati se može samo iz klase.
 
-            public string Prezime;
+            public string Prezime
+            {
+                get;
+                protected set;
+            }
 
 
-            // TODO:001 Javno dostupno polje Ime nadomjestiti svojstvom (property) koje se izvan klase može čitati i zadavati, ali prilikom zadavanja treba onemogućiti
+            // :001 Javno dostupno polje Ime nadomjestiti svojstvom (property) koje se izvan klase može čitati i zadavati, ali prilikom zadavanja treba onemogućiti
             // zadavanje nul-referencom ili praznim znakovnim nizom. U tim slučajevima treba baciti iznimke tipa ArgumentNullException, odnosno ArgumentException.
 
-            public string Ime;
+            private string ime;
+            public string Ime
+            {
+                get => ime;
+                set
+                {
+                    if (value == null)
+                        throw new ArgumentNullException(nameof(Ime));
+                    if (value.Trim().Length == 0)
+                        throw new ArgumentException("Ime ne smije biti prazno", nameof(Ime));
+                    ime = value;
+                }
+            }
 
 
             // TODO:002 Javno dostupno polje DatumRođenja nadomjestiti svojstvom (property) koje se izvan klase može čitati i zadavati, ali za slučaj zadavanja
             // datuma većeg od trenutnog treba baciti iznimku tipa ArgumentOutOfRangeException. U pozivajućem kodu staviti odgovarajući kod za hvatanje
             // iznimke koji će u slučaju iznimke ispisati odgovarajuću poruku.
 
-            public DateTime DatumRođenja;
-
-        }
-
-        // TODO:004 Definirati da je klasa OsobaSPromjenivimPrezimenom izvedena iz klase Osoba, ukloniti polja Ime i Prezime iz klase OsobaSPromjenivimPrezimenom 
-        // te iz konstruktora inicijalizirati članove bazne klase.
-        public class OsobaSPromjenivimPrezimenom
-        {
-            public string Ime;
-            public string Prezime;
-
-            public OsobaSPromjenivimPrezimenom(string ime, string prezime)
-            { }
-
-            // TODO:005 Napraviti potrebne promjene svojstva Prezime u baznoj klasi Osoba da se iz metode UdajSe može promijeniti prezime osobe.
-            // TODO:006 Napisati unutar metode UdajSe kod kojim se mijenja prezime osobe.
-            public void UdajSe(string prezimePartnera)
+            public DateTime datumRođenja;
+            public DateTime DatumRođenja
             {
+                get => datumRođenja;
+                set
+                {
+                    //
+                }
             }
-        }
 
 
-        static void Main(string[] args)
-        {
-            // TODO:003 Provjeriti donjim kodom ispravnost promjena (zakomentirati naredbe koje će uzrokovati pogrešku pri prevođenju nakon promjena u klasi Osoba).
 
-            Console.WriteLine("*** Osoba ***");
+            // :004 Definirati da je klasa OsobaSPromjenivimPrezimenom izvedena iz klase Osoba, ukloniti polja Ime i Prezime iz klase OsobaSPromjenivimPrezimenom 
+            // te iz konstruktora inicijalizirati članove bazne klase.
+            public class OsobaSPromjenivimPrezimenom : Osoba
+            {
+                //public string Ime;
+                //public string Prezime;
 
-            Osoba o1 = new Osoba("Oliver", "Mlakar");
-            Console.WriteLine($"{o1.Ime} {o1.Prezime}");
+                public OsobaSPromjenivimPrezimenom(string ime, string prezime) : base(ime, prezime)
+                { }
 
-            o1.DatumRođenja = new DateTime(1933, 4, 13);
-            Console.WriteLine(o1.DatumRođenja.ToShortDateString());
+                // TODO:005 Napraviti potrebne promjene svojstva Prezime u baznoj klasi Osoba da se iz metode UdajSe može promijeniti prezime osobe.
+                // TODO:006 Napisati unutar metode UdajSe kod kojim se mijenja prezime osobe.
+                public void UdajSe(string prezimePartnera)
+                {
+                    Prezime = prezimePartnera;
+                }
+            }
 
-            o1.Ime = "Pero";
-            o1.Prezime = "Kvrgić";
-            Console.WriteLine($"{o1.Ime} {o1.Prezime}");
 
-            o1.DatumRođenja = new DateTime(2025, 4, 13);
-            Console.WriteLine(o1.DatumRođenja.ToShortDateString());
+            static void Main(string[] args)
+            {
+                // :003 Provjeriti donjim kodom ispravnost promjena (zakomentirati naredbe koje će uzrokovati pogrešku pri prevođenju nakon promjena u klasi Osoba).
 
-            // TODO:007 Pokrenuti program i provjeriti ispis za OsobuSPromjenivimPrezimenom.
-            // TODO:008 Pokrenuti testove (5 testova u grupi "TestDefinicijeSvojstva" mora proći).
-            Console.WriteLine();
-            Console.WriteLine($"*** OsobaSPromjenivimPrezimenom ***");
+                Console.WriteLine("*** Osoba ***");
 
-            OsobaSPromjenivimPrezimenom o2 = new OsobaSPromjenivimPrezimenom("Nives", "Celzius");
-            Console.WriteLine($"Prije udaje: {o2.Ime} {o2.Prezime}");
-            o2.UdajSe("Fahrenheit");
-            Console.WriteLine($"Nakon udaje: {o2.Ime} {o2.Prezime}");
+                Osoba o1 = new Osoba("Oliver", "Mlakar");
+                Console.WriteLine($"{o1.Ime} {o1.Prezime}");
 
-            Console.WriteLine("GOTOVO!!!");
-            Console.ReadKey(true);
+                o1.DatumRođenja = new DateTime(1933, 4, 13);
+                Console.WriteLine(o1.DatumRođenja.ToShortDateString());
+
+                o1.Ime = "Pero";
+                //o1.Prezime = "Kvrgić";
+                Console.WriteLine($"{o1.Ime} {o1.Prezime}");
+
+                o1.DatumRođenja = new DateTime(2025, 4, 13);
+                Console.WriteLine(o1.DatumRođenja.ToShortDateString());
+
+                // :007 Pokrenuti program i provjeriti ispis za OsobuSPromjenivimPrezimenom.
+                // :008 Pokrenuti testove (5 testova u grupi "TestDefinicijeSvojstva" mora proći).
+                Console.WriteLine();
+                Console.WriteLine($"*** OsobaSPromjenivimPrezimenom ***");
+
+                OsobaSPromjenivimPrezimenom o2 = new OsobaSPromjenivimPrezimenom("Nives", "Celzius");
+                Console.WriteLine($"Prije udaje: {o2.Ime} {o2.Prezime}");
+                o2.UdajSe("Fahrenheit");
+                Console.WriteLine($"Nakon udaje: {o2.Ime} {o2.Prezime}");
+
+                Console.WriteLine("GOTOVO!!!");
+                Console.ReadKey(true);
+            }
         }
     }
 }
