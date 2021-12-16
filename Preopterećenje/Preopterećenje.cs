a6286493270f3ce7e61614ea79fcd90285430043
Ivan Čulina <72503859+ivculina@users.noreply.github.com>
2021-12-16 20:27:35 +0100

    resolve property overloading

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -10,9 +10,11 @@ namespace Vsite.CSharp.Svojstva
             get { return Math.PI; }
         }
 
-        // TODO:020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
+        // :020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
+        //public void Preopterećenosvojstvo { get; set; }
 
-        // TODO:021 Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
+        // :021 Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
+        //public void Preopterećenosvojstvo { get; set; }
 
         static void Main(string[] args)
         {
