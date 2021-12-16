0b516170e60a124a384f70d8f1e0a40fc569c508
Klara Blašković <72518590+klblasko@users.noreply.github.com>
2021-12-16 20:27:12 +0100

    Resolve property overrloading

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -10,9 +10,11 @@ namespace Vsite.CSharp.Svojstva
             get { return Math.PI; }
         }
 
-        // TODO:020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
+        // 020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
+        //public void PreopterećenoSvojstvo { get; set; }
 
-        // TODO:021 Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
+        // 021 Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
+        //public int PreopterećenoSvojstvo { get; set; }
 
         static void Main(string[] args)
         {
