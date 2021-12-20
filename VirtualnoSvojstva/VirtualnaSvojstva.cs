109a0ff0b981fb7166febc0df9e8653bf138cad0
Luka Šestak <lusestak@vsite.hr>
2021-12-20 21:49:49 +0100

    luka_sestak

--- VirtualnoSvojstva/VirtualnaSvojstva.cs
+++ VirtualnoSvojstva/VirtualnaSvojstva.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 
+
 namespace Vsite.CSharp.Svojstva
 {
     class VirtualnaSvojstva
@@ -26,16 +27,20 @@ namespace Vsite.CSharp.Svojstva
                 Godina = godina;
             }
 
-            // TODO:010 Nadglasati (overrideati) svojstvo Identifikacija tako da get metoda vraća znakovni niz oblika: "Pero, 2. godina".
 
+            // 010 Nadglasati (overrideati) svojstvo Identifikacija tako da get metoda vraća znakovni niz oblika: "Pero, 2. godina".
+            public override string Identifikacija
+            {
+                get { return $"{base.Identifikacija}, {Godina}. godina"; }
+            }
 
 
             public int Godina { get; set; }
         }
 
-        // TODO:011 Pokrenuti program i provjeriti ispis.
+        // 011 Pokrenuti program i provjeriti ispis.
 
-        // TODO:012 Pokrenuti testove (test u grupi "TestVirtualnogSvojstva" mora proći).
+        // 012 Pokrenuti testove (test u grupi "TestVirtualnogSvojstva" mora proći).
 
         static void Main(string[] args)
         {
