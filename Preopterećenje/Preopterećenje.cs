6cdcea691d29d12a5673bcd5d38d41768ece64e3
Kristian Mesić <kristian.mesic@king-ict.eu>
2024-01-11 19:22:48 +0100

    - do Preopterecenje

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -8,9 +8,14 @@
             get { return Math.PI; }
         }
 
-        // TODO:020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod.
+        // :020 Zadati svojstvo VoidSvojstvo tipa void i pokušati prevesti kod. -> ne može biti void
+        //public void VoidSvojstvo
+        //{
+        //    get; set;
+        //}
 
-        // TODO:021 Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
+        // :021 Dodati novo svojstvo PreopterećenoSvojstvo koje će biti tipa int i pokušati prevesti kod.
+        public int PreopterecenoSvojstvo {  get { return 0; } }
 
         static void Main()
         {
